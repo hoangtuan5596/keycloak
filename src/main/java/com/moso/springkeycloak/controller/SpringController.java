180b576522df252da842f0d5f2c1037d47c31afd
Tuan VH <hoangtuan5596@gmail.com>
2020-07-08 13:49:00 +0700

    Error map path
    Signed-off-by: Tuan VH <hoangtuan5596@gmail.com>

--- src/main/java/com/moso/springkeycloak/controller/SpringController.java
+++ src/main/java/com/moso/springkeycloak/controller/SpringController.java
@@ -1,25 +1,38 @@
 package com.moso.springkeycloak.controller;
 
+import com.moso.springkeycloak.model.User;
 import com.moso.springkeycloak.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.security.Principal;
+
+@Controller
 public class SpringController {
+    Logger logger = LoggerFactory.getLogger(SpringController.class);
+
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping(path ="/")
-    public String index () {
+    @GetMapping(path = "/")
+    public String index() {
         return "index";
     }
 
+    /*
+    * Get list details user before login success.
+    */
     @GetMapping(path = "/users")
-    public String user(Model model){
-        System.out.println("Vào đây");
-       // model.addAttribute("user", userRepository.detailsUser());
+    public String user(Principal principal, Model model) {
+        logger.info("USER");
+        Iterable<User> users = userRepository.detailsUser();
+        model.addAttribute("users", users);
+        model.addAttribute("usersName", principal.getName());
+        logger.info("USER" + users);
         return "user";
     }
 
